2d998e3f3cad868a54169ce74688fa620c7b24bb
Sam Gunner <samgunnerpc@live.com>
2015-12-04 23:08:06 +0000

    filler changes

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -2,9 +2,11 @@
 	A JavaScript file to fetch and display the new articles, upon loading the page or clicking of the 'play again' button.
 */
 
-var xhttp;
-xhttp = new XMLHttpRequest();
+
 
 var getArticle = function() {
-	
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+
+	xhttp.open("POST", "http://onionornot.net/fetch.php", true)
 }

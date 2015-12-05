3421419e8d41a41afa7c6fc021f84b07ae1a6d9b
Sam Gunner <samgunnerpc@live.com>
2015-12-05 18:47:00 +0000

    UPDATE - Now adds to database and gets articles

--- assets/php/fetch.php
+++ assets/php/fetch.php
@@ -2,31 +2,94 @@
 
   //A PHP script to fetch, return and possibly store the latest headlines from the /r/onion and /r/notonion subreddits.
 
-function getTitles() {
-	$feed = implode(file('http://reddit.com/r/nottheonion.rss?limit=100'));
-	$xml = simplexml_load_string($feed);
-	$json = json_encode($xml);
-	$array = json_decode($json,TRUE);
-	$feed = implode(file('http://reddit.com/r/theonion.rss?limit=100'));
-	$xml = simplexml_load_string($feed);
-	$json = json_encode($xml);
-	$array2 = json_decode($json,TRUE);
 
-	for ($i = 1; $i < count(array2['channel']['item']); $i++) {
-		array_push($array['channel']['item'], $array2['channel']['item'][$i]);
+ob_start();
+
+$conn = new mysqli("localhost", "root", "Minecon2013", "onionornot");
+if ($conn->connect_error) {
+	echo "Connection failed. Please reload page.";
+}
+
+function getTitles() {
+	$endArray = array();
+
+	$feed = file_get_contents('http://reddit.com/r/nottheonion.json?limit=10');
+	$array = json_decode($feed, true)['data']['children'];
+	foreach ($array as $post) {
+		if (!($post['data']['domain'] === "self.nottheonion")) {
+			$post['data']['preview'] = null;
+			$post['data']['resolutions'] = null;
+			array_push($endArray, $post);
+		}
 	}
 
-	return $array;
+	$feed = file_get_contents('http://reddit.com/r/TheOnion.json?limit=10');
+	$array = json_decode($feed, true)['data']['children'];
+	foreach ($array as $post) {
+		if (!($post['data']['domain'] === "self.theonion")) {
+			$post['data']['preview'] = null;
+			$post['data']['resolutions'] = null;
+			array_push($endArray, $post);
+		}
+	}
+
+	return $endArray;
 }
 
-function getArticle() { //Fetch article, display to user.
-	$conn = new mysqli("127.0.0.1", "database", "Password1234567890", "articles");
-	$query = "SELECT column FROM `allowed`
-	ORDER BY RAND()
-	LIMIT 1";
+function addArticle($art, $conn) {
+	$endArr = array();
+	$art = $art['data'];
+	$endArr['URL'] = $art['url'];
+	$endArr['Title'] = $art['title'];
+	if ($art['subreddit'] === "nottheonion") {
+		$endArr['Onion'] = 0;
+	}
+	else {
+		$endArr['Onion'] = 1;
+	}
+	$query = "INSERT INTO `articles_enabled` (URL, Title, Onion, NSFW) VALUES ('".$endArr['URL']."', '".$endArr['Title']."', ".$endArr['Onion'].", 0)";
+	$conn->query($query);
+}
+
+function articleExists($art, $conn) { //Returns boolean, true if it exists, false if not.
+	$query = "SELECT * FROM `articles_enabled` WHERE `URL` = '".$art['data']['url']."'";
+	$results1 = $conn->query($query);
+	$query = "SELECT * FROM `articles_enabled` WHERE `Title` = '".$art['data']['title']."'";
+	$results2 = $conn->query($query);
+	if (mysqli_num_rows($results1) >= 1 and mysqli_num_rows($results2) >= 1) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function getArticle($conn) { //Fetch article, display to user.
+
+	$query = "SELECT * FROM `articles_enabled` ORDER BY RAND() LIMIT 1";
 
 	$result = $conn->query($query);
-	print_r($result);
+	foreach ($result as $rec) {
+		$str = $rec['ID'] . "," . $rec['URL'] . "," . $rec["Title"] . "," . $rec["Onion"] . "," . $rec["NSFW"];
+		return $str;
+	}
 }
+
+
+
+
+
+echo getArticle($conn);
+header("Content-Length: ".ob_get_length());
+header("Connection: close");
+ob_end_flush();
+
+$array = getTitles();
+foreach ($array as $rec) {
+	if (!articleExists($rec, $conn)) {
+		addArticle($rec, $conn);
+	}
+}
+
+
 
 ?>
